1ab044a6dc0fda9e79b258c9751cfc913b084ddf
Herlan12 <musikgratissaja@gmail.com>
2016-09-11 19:15:20 +0700

    playlist update 01

--- my-player/playlist.js
+++ my-player/playlist.js
@@ -14,64 +14,104 @@ $(document).ready(function(){
 			title:"Jangan Ingin Menang Sendiri",
 			artist:"Betharia Sonata",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aTGFTY3A2RFZ1bGM",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Selamat Tinggal",
 			artist:"Broery Marantika",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7abGh0Qk5IWkx6ZGs",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Jangan Dusta Di Antara Kita",
 			artist:"Broery Marantika feat Dewi Yull",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aUnBVU050dU0wV28",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Aku Sayang Kamu",
 			artist:"Cindy Claudia",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aWW1NR1RHU1VhdUk",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Damai Bersamamu",
 			artist:"Chrisye",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7adW4tc1FnZlpTdmM",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Sampai Hati",
 			artist:"Dessy Ratnasari",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aN2RMZHFya2pXOW8",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Kini Baru Kau Rasa",
 			artist:"Dewi Yull",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aUG8wY3NjQjk0Qmc",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Apa Salah dan Dosaku",
 			artist:"Dllyod",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aWGZfMDBCa3JvZU0",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
 			title:"Laut",
 			artist:"Doel Sumbang",
 			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aZHBmVm1IRTBya28",
-			poster: "http://www.jplayer.org/video/poster/Finding_Nemo_Teaser_640x352.png"
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
 		},
 		{
-			title:"Incredibles Teaser",
-			artist:"Pixar",
-			m4v: "http://www.jplayer.org/video/m4v/Incredibles_Teaser.m4v",
-			ogv: "http://www.jplayer.org/video/ogv/Incredibles_Teaser.ogv",
-			webmv: "http://www.jplayer.org/video/webm/Incredibles_Teaser.webm",
-			poster: "http://www.jplayer.org/video/poster/Incredibles_Teaser_640x272.png"
-		}
+			title:"Camelia I",
+			artist:"Ebiet G Ade",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aUlJDN2I0UzF6MDQ",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
+		{
+			title:"Lagu Untuk Sebuah Nama",
+			artist:"Ebiet G Ade",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aSlktSkU4eC1qcDg",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
+		{
+			title:"Kini Kusadari",
+			artist:"Eddie Silitonga",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aVnNjZ2ViM2RGQ28",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
+		{
+			title:"Teluk Bayur",
+			artist:"Ernie Djohan",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aSFZySGNFMkN2bGs",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
+		{
+			title:"Ikan Lautpun Menari",
+			artist:"Farid Hardja",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7acDZqdmFlTC1fZzA",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
+		{
+			title:"Lautpun Menangis Untukku",
+			artist:"Fitri Handayani",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7adzZPV0NLYkFMTW8",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
+		{
+			title:"Lelaki dan Rembulan",
+			artist:"Franky and Jane",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7aTjdNaXh3TTNjUXc",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
+		{
+			title:"Kugadaikan Cintaku",
+			artist:"Gombloh",
+			m4v: "https://docs.google.com/uc?export=download&id=0B5nAHiC-ar7acHJyY0JoV1RIVE0",
+			poster: "http://1.bp.blogspot.com/-oZYu6dwA-TM/V9JQ53m9B0I/AAAAAAAADh4/oDCW0hAlN-05esv2n_M6b6GWQPd1MYRQwCK4B/s1600/player%2Bcopy.jpg"
+		},
 	], {
 		swfPath: "../../dist/jplayer",
 		supplied: "webmv, ogv, m4v",
